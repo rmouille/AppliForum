004ffa4b526068467841b7b66de16e34ba235cec
unknown <rmouille@LFR410575.corp.capgemini.com>
2015-12-10 18:10:35 +0100

    Creation du service entreprise, du json et code de la liste des entreprises

--- www/modules/listeEntreprise/listeEntreprise.js
+++ www/modules/listeEntreprise/listeEntreprise.js
@@ -1,4 +1,7 @@
 angular.module("AppliForum.ListeEntreprise", [])
-    .controller('ListeEntrepriseCtrl', function($scope) {
-        $scope.t = "test";
+    .controller('ListeEntrepriseCtrl', function($scope, Entreprise) {
+        $scope.t = "testouilles";
+        Entreprise.init();
+        $scope.entreprises = Entreprise.getList();
+
     })
