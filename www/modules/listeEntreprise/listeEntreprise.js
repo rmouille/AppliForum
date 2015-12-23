01e2fbe60adea7856b365f3eca4f7424b916ba4b
unknown <rmouille@LFR410575.corp.capgemini.com>
2015-12-23 22:43:56 +0100

    ajout de la recherche sur la liste d'entreprises, ajout de la liste des postes, update du json entreprise et passage de la bar en dark

--- www/modules/listeEntreprise/listeEntreprise.js
+++ www/modules/listeEntreprise/listeEntreprise.js
@@ -1,6 +1,17 @@
 angular.module("AppliForum.ListeEntreprise", [])
     .controller('ListeEntrepriseCtrl', function($scope, Entreprise) {
         Entreprise.init();
+
+        $scope.search = {
+            text: ''
+        };
         $scope.entreprises = Entreprise.getList();
 
-    })
+        $scope.searchCompany = function(data) {
+            if ((angular.lowercase(data.nom).indexOf(angular.lowercase($scope.search.text)) != -1) || (angular.lowercase(data.type).indexOf(angular.lowercase($scope.search.text)) != -1)) {
+                return true;
+            }
+            return false;
+        };
+
+    });
